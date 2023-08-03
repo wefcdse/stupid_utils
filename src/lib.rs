4d5db5a309a55dbdc1099632d731694a49029da9
eqf <3371348169@qq.com>
2023-08-03 14:47:24 +0800

    change doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
+//! # decription
+//! A crate that provides some simple and maybe stupid or useful tools
+//!
+//! # Example
+//! ```
+//! use std::collections::HashMap;
+//! use stupid_utils::predule::*;
+//!
+//! let a = HashMap::new().mutable_init(|m| {
+//!     m.insert(1, 4.box_up());
+//!     m.insert(
+//!         2,
+//!         Some(9)
+//!             .map_value(|v| match v {
+//!                 Some(v) => v,
+//!                 None => 3,
+//!             })
+//!             .box_up(),
+//!     );
+//!     let cond = true;
+//!     m.insert(cond.select(3, 4), select(cond, 3, 4).box_up());
+//! });
+//!
+//! let b = {
+//!     let mut m = HashMap::new();
+//!     m.insert(1, Box::new(4));
+//!     m.insert(
+//!         2,
+//!         Box::new({
+//!             let v = Some(9);
+//!             match v {
+//!                 Some(v) => v,
+//!                 None => 3,
+//!             }
+//!         }),
+//!     );
+//!     let cond = true;
+//!     m.insert(if cond { 3 } else { 4 }, Box::new(if cond { 3 } else { 4 }));
+//!     m
+//! };
+//!
+//! assert_eq!(a, b);
+//! ```
+
 pub mod predule {
     pub use crate::arc_mutex::ArcMutex;
     pub use crate::arc_mutex_new::arc_mutex_new;
